9ad5ba018e00496af06ca63054b071643ca7f75f
lukaszkuchta <lukaszkuchta10@gmail.com>
2022-11-27 09:10:24 +0100

    koniec

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -99,7 +99,66 @@ public class Main {
                     session.remove(pojazd);
                 }
                 transaction.commit();
+            }catch (Exception e){
+                System.err.println("Błąd bazy: " + e);
             }
+        }else if (odpowiedz.equalsIgnoreCase("aktualizuj")){
+            try(Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()){
+                Transaction transaction = session.beginTransaction();
+                System.out.println("Podaj id pojazdu którego chcesz aktualizować:");
+                String id = scanner.nextLine();
+                Long pojazdId = Long.parseLong(id);
+
+                Pojazd pojazd = session.get(Pojazd.class, pojazdId);
+
+                if (pojazd == null) {
+                    System.err.println("Student nie istnieje.");
+                } else {
+                    System.out.println("podaj marke");
+                    String marka = scanner.nextLine();
+
+                    System.out.println("podaj moc");
+                    Double moc = Double.parseDouble(scanner.nextLine());
+
+                    System.out.println("podaj kolor");
+                    String kolor = scanner.nextLine();
+
+                    Integer rokProdukcji = null;
+                    do{
+                        System.out.println("podaj rok produkcji");
+                        rokProdukcji = Integer.parseInt(scanner.nextLine());
+                    }while (!(rokProdukcji >= 1990 && rokProdukcji <= 2020));
+
+                    System.out.println("czy jest elektryczny? (tak / nie)");
+                    Boolean elektryczny = false;
+                    String odpowiedzCzyElektryczny = scanner.nextLine();
+                    if(odpowiedzCzyElektryczny.equalsIgnoreCase("tak")){
+                        elektryczny = true;
+                    } else if (odpowiedzCzyElektryczny.equalsIgnoreCase("nie")) {
+                        elektryczny = false;
+                    }else {
+                        System.out.println("niepoprawna odpowiedzi i nie jest elektryczny");
+                        elektryczny = false;
+                    }
+
+                    Pojazd pojazdAktualizowany = Pojazd.builder()
+                            .marka(marka)
+                            .moc(moc)
+                            .kolor(kolor)
+                            .rokProdukcji(rokProdukcji)
+                            .elektryczny(elektryczny)
+                            .id(pojazdId)
+                            .build();
+
+                    session.merge(pojazdAktualizowany);
+
+                    transaction.commit();
+                }
+            }catch (Exception e){
+                System.err.println("Błąd bazy: " + e);
+            }
+        } else {
+            System.err.println("niepoprawna komenda");
         }
 
 
